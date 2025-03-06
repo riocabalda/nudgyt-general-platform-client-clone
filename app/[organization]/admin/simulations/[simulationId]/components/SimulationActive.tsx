1e5b88e7abb3cd3eebe90b69e39785b6c5d857f5
riocabalda <159091515+riocabalda@users.noreply.github.com>
2025-03-06 15:19:55 +0800

    feat: websocket integration.

--- app/[organization]/admin/simulations/[simulationId]/components/SimulationActive.tsx
+++ app/[organization]/admin/simulations/[simulationId]/components/SimulationActive.tsx
@@ -1,12 +1,33 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Simulation } from '@/app/(shared)/services/learner/simulationService'
 import RightSidebar from './RightSidebar'
 import SimulationE3dsIframe from './SimulationE3dsIframe'
 import NavigationPanel from '@/app/(shared)/components/admin-trainer-learner-shared/simulations/[simulationId]/components/NavigationPanel'
-
+import authTokenService from '@/app/(shared)/services/authTokenService'
+import useSimulationFormStore from '@/app/(shared)/components/admin-trainer-learner-shared/simulations/[simulationId]/hooks/useSimulationFormStore'
+import serverConfig from '@/app/(shared)/config/serverConfig'
+import io from 'socket.io-client'
 function SimulationActive({ simulation }: { simulation: Simulation }) {
+  const accessToken = authTokenService.getAccessToken()
+
+  const { resetFormState } = useSimulationFormStore()
+
+  useEffect(() => {
+    const socket = io(serverConfig.socketUrl, {
+      reconnection: true,
+      auth: {
+        accessToken: `Bearer ${accessToken}`,
+        payload: { simulationId: simulation._id }
+      }
+    })
+
+    return () => {
+      socket.disconnect()
+      resetFormState()
+    }
+  }, [])
   return (
     <section className='lg:p-6 lg:pb-0'>
       <div className='relative flex flex-col lg:flex-row lg:gap-6 overflow-hidden lg:max-w-[1920px] lg:mx-auto lg:px-0 '>
